a0c7f781e245708e13bd83c53be55379c9308086
Ģirts Bukovskis <girtsbuk@gmail.com>
2020-07-04 10:51:46 +0300

    file updates

--- src/main/java/credit/loan/application/module/User.java
+++ src/main/java/credit/loan/application/module/User.java
@@ -1,10 +1,77 @@
 package credit.loan.application.module;
 
 
+import com.sun.xml.bind.v2.model.core.ID;
+import sun.security.x509.CertificateIssuerName;
+
 import javax.persistence.Entity;
 
 @Entity
 public class User {
+    private String id;
+    private String name;
+    private String surname;
+    private String email;
+    private String personalCode;
+    private String phoneNumber;
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPersonalCode() {
+        return personalCode;
+    }
+
+    public void setPersonalCode(String personalCode) {
+        this.personalCode = personalCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", personalCode='" + personalCode + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
